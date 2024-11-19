b9176d352db0425827b6909dee97e73689c25ede
drLacheheb <yacinolach@gmail.com>
2024-11-19 23:13:50 +0100

    Create EmployeeSaverUseCase class for handling employee saving logic

---
+++ src/use_cases/employee_saver/use_case.py
@@ -0,0 +1,18 @@
+from src.entities.values.employee import Employee
+from src.use_cases.employee_saver.values.response_dto import ResponseDTO
+
+
+class EmployeeSaverUseCase:
+    def __init__(self, employee_saver_repository):
+        self.employee_saver_repository = employee_saver_repository
+
+    def execute(self, employee) -> ResponseDTO:
+        try:
+            self.employee_saver_repository.save(
+                Employee(first_name="first name", last_name="last name")
+            )
+        except Exception:
+            return ResponseDTO(
+                executed=False,
+                exception_occurred=True
+            )
