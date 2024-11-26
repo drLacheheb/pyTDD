4ec55f9ca911e50d891aa1a87f90e8cb17280e04
elhareth0609 <elhareh0609@gmail.com>
2024-11-26 15:07:34 +0100

    Refactoring

--- src/use_cases/employee_saver/use_case.py
+++ src/use_cases/employee_saver/use_case.py
@@ -1,11 +1,16 @@
 from src.entities.values.employee import Employee
+from src.use_cases.employee_saver.dependencies.employee_saver_repository_interface import (
+    EmployeeSaverRepositoryInterface,
+)
 from src.use_cases.employee_saver.values.employee_dto import EmployeeDTO
 from src.use_cases.employee_saver.values.response_dto import ResponseDTO
 
 
 class EmployeeSaverUseCase:
-    def __init__(self, employee_saver_repository):
-        self.employee_saver_repository = employee_saver_repository
+    def __init__(self, employee_saver_repository: EmployeeSaverRepositoryInterface):
+        self.employee_saver_repository: EmployeeSaverRepositoryInterface = (
+            employee_saver_repository
+        )
 
     def execute(self, employee_dto: EmployeeDTO) -> ResponseDTO:
         def save_to_repository(_employee_dto: EmployeeDTO) -> None:
