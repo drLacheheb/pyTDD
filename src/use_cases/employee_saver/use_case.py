ee384814f112d8210fccf06d750c439fa1b64224
elhareth0609 <elhareh0609@gmail.com>
2024-11-26 14:37:04 +0100

    Refactoring

--- src/use_cases/employee_saver/use_case.py
+++ src/use_cases/employee_saver/use_case.py
@@ -1,4 +1,5 @@
 from src.entities.values.employee import Employee
+from src.use_cases.employee_saver.values.employee_dto import EmployeeDTO
 from src.use_cases.employee_saver.values.response_dto import ResponseDTO
 
 
@@ -6,10 +7,12 @@ class EmployeeSaverUseCase:
     def __init__(self, employee_saver_repository):
         self.employee_saver_repository = employee_saver_repository
 
-    def execute(self, employee) -> ResponseDTO:
+    def execute(self, employee_dto: EmployeeDTO) -> ResponseDTO:
         try:
             self.employee_saver_repository.save(
-                Employee(first_name=employee.first_name, last_name=employee.last_name)
+                Employee(
+                    first_name=employee_dto.first_name, last_name=employee_dto.last_name
+                )
             )
 
         except Exception:
